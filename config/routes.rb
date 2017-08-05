fe788581b0803e6daf6a9c193e2d0dc2535c965f
jkxruby <jkxruby@gmail.com>
2017-08-05 10:01:03 +0800

    modify the admin/jobs/resumes routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,17 @@ root 'jobs#index'
 
 resources :jobs do
   resources :resumes
-end 
+end
 
 namespace :admin do
-  resources :jobs do
+  resources :jobs do 
   member do
     post :publish
     post :hide
   end
+  resources :resumes
 end
 end
+
 
 end
