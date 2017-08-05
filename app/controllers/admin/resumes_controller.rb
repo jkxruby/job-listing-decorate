b5ce269207799be9d50523a049f011626da070ea
jkxruby <jkxruby@gmail.com>
2017-08-05 10:28:29 +0800

    admin can see resume list

--- app/controllers/admin/resumes_controller.rb
+++ app/controllers/admin/resumes_controller.rb
@@ -1,2 +1,10 @@
 class Admin::ResumesController < ApplicationController
+before_action :authenticate_user!
+before_action :authenticate_is_admin!
+
+def inex
+  @job = Job.find(params[:job_id])
+  @resumes = @job.resumes.order('created_at DESC')
+end
+
 end
